d7b413e1626be18e7d6ea6c36dae45d9c36646fe
IVGos <greengesty@gmail.com>
2025-02-14 10:53:37 +0300

    nav, reg

---
+++ client/src/pages/Nav/Nav.jsx
@@ -0,0 +1,40 @@
+import styles from './Nav.module.css';
+import { NavLink } from 'react-router';
+import { useNavigate } from 'react-router';
+
+export default function Nav() {
+  let navigate = useNavigate();
+
+  return (
+    <nav className={styles.container}>
+      
+
+
+      <NavLink
+        to='/registration'
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        страничка регистарции
+      </NavLink>      
+      
+      
+
+      <NavLink
+        to='/themes'
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        Темы
+      </NavLink>
+
+      <NavLink
+        to='/input'
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        Выйти
+      </NavLink>
+      {/* <button type='button' onClick={() => navigate('/test')}>
+        результат
+      </button> */}
+    </nav>
+  );
+}
